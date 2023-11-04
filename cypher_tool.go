ae56eb1d239d3fe132954beb3407ad1a09f0523c
LambO <vladislavaleksandrov96@gmail.com>
2023-11-04 15:09:32 +0200

    cypher_tool.go

--- cypher_tool.go
+++ cypher_tool.go
@@ -8,13 +8,14 @@ func main() {
 
 func getInput() (toEncrypt bool, encoding string, message string) {
 	fmt.Println("Welcome to the Cypher Tool!")
+	// Command for output of the welcome phrase
 	fmt.Println("Select operation (1/2):\n 1. Encrypt. \n 2. Decrypt.")
 	var SelectOp int
 	fmt.Scan(&SelectOp)
 	if SelectOp == 1 {
 		toEncrypt = true
 	} else if SelectOp == 2 {
-		toEncrypt = true
+		toEncrypt = false
 	}
 	fmt.Println("Select cypher (1/2):\n 1. ROT13. \n 2. Reverse")
 	fmt.Scan(&encoding)
